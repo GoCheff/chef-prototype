87f77c3f2c0671f7be4fa7b5b1df00ce6a3fce62
pedrokappes <pedrokappes@gmail.com>
2023-06-06 19:57:00 -0300

    Finalizando pagina home e reestruturando

--- views/home/home.js
+++ views/home/home.js
@@ -1,6 +1,17 @@
-var btnMaisDetalhes = document.querySelector('.btn-mais-detalhes');
-var detalhesPedido = document.querySelector('.detalhes-pedido');
+var btnPedidos = document.getElementById("btnpedidos");
+var btnTodosPedidos = document.getElementById("btntodospedidos");
+var btnReceitas = document.getElementById("btnreceitas");
+var btncadastrarreceitas = document.getElementById("btnreceitas");
 
-btnMaisDetalhes.addEventListener('click', function() {
-  detalhesPedido.classList.toggle('mostrar-detalhes');
-});
+btnPedidos.addEventListener("click", () => {
+    const currentUrl = window.location.href;
+
+    if (!currentUrl.includes("gocheff.github.io")) {
+        window.location.assign("../pedidos/pedidos.html");
+        return;
+    }
+
+    window.location.assign("/chef-prototype/views/pedidos/pedidos.html");
+})
+
+
