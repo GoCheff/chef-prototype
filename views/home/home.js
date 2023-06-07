de2e5deac02fbe67afaf0659ca2eded4a222858a
pedrokappes <pedrokappes@gmail.com>
2023-06-06 22:17:05 -0300

    Adição da pagina receitas, finalizando pedidos e verificar pedidos

--- views/home/home.js
+++ views/home/home.js
@@ -1,5 +1,5 @@
 var btnPedidos = document.getElementById("btnpedidos");
-var btnTodosPedidos = document.getElementById("btntodospedidos");
+var btnVerificarPedidos = document.getElementById("btnverificarpedidos");
 var btnReceitas = document.getElementById("btnreceitas");
 var btncadastrarreceitas = document.getElementById("btnreceitas");
 
@@ -12,6 +12,28 @@ btnPedidos.addEventListener("click", () => {
     }
 
     window.location.assign("/chef-prototype/views/pedidos/pedidos.html");
+})
+
+btnVerificarPedidos.addEventListener("click", () => {
+    const currentUrl = window.location.href;
+
+    if (!currentUrl.includes("gocheff.github.io")) {
+        window.location.assign("../verificarPedidos/verificarPedidos.html");
+        return;
+    }
+
+    window.location.assign("/chef-prototype/views/verificarPedidos/verificarPedidos.html");
+})
+
+btnReceitas.addEventListener("click", () => {
+    const currentUrl = window.location.href;
+
+    if (!currentUrl.includes("gocheff.github.io")) {
+        window.location.assign("../receitas/receitas.html");
+        return;
+    }
+
+    window.location.assign("/chef-prototype/views/receitas/receitas.html");
 })
 
 
